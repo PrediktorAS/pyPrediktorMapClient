1b38074e83087ab93f34a3aaf892610c3905643f
Andreas Lydersen <andreas.lydersen@prediktor.com>
2022-09-27 00:17:31 +0200

    Removed unused test criteria

--- tests/test_shared.py
+++ tests/test_shared.py
@@ -6,7 +6,7 @@ from pyprediktormapclient.shared import request_from_api
 
 class AnalyticsHelperTestCase(unittest.TestCase):
     def test_requests_with_unsupported_method(self):
-        with pytest.raises(Exception) as e_info:
+        with pytest.raises(Exception):
             request_from_api(rest_url="No_valid_url", method="PUT", endpoint="/")
 
 
