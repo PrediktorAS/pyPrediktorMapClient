8d72dfda2a41e176ef6962c0ce006096fb1f9a47
Pavel Tashev <pta>
2023-11-14 17:52:39 +0200

    For DWH tests set proper test names following when-then convention. Also, the test testing successfull creation of DWH instance, we mock pyodbc connection instead of method written by us in that way we follow the rule to test behaviour but not implementation makingour tests less frigile.

--- tests/test_dwh.py
+++ tests/test_dwh.py
@@ -3,32 +3,37 @@ import random
 import string
 from pyprediktormapclient.dwh import DWH
 
+'''
+Helpers
+'''
+class mock_pyodbc_connection:
+        def __init__(self, connection_string):
+            pass
+
+        def cursor(self):
+            pass
+
 def grs():
+    '''Generate a random string.'''
     return "".join(random.choices(string.ascii_uppercase + string.digits, k=10))
 
 '''
 __init__
 '''
-def test_database_operations_init_success(monkeypatch):
+def test_init_when_instantiate_dwh_then_instance_is_created(monkeypatch):
     # Mock the database connection
-    def mock_connect(self):
-        pass
-
     monkeypatch.setattr(
-        "pyprediktormapclient.dwh.DWH._DWH__connect", mock_connect
+        "pyprediktormapclient.dwh.pyodbc.connect",
+        mock_pyodbc_connection
     )
 
     dwh = DWH(grs(), grs(), grs(), grs())
     assert dwh is not None
 
-'''
-connect
-'''
-def test_connect_to_database_using_pyodbc_no_drivers(monkeypatch):
+def test_init_when_instantiate_dwh_but_no_pyodbc_drivers_available_then_throw_exception(monkeypatch):
     # Mock the absence of ODBC drivers
     monkeypatch.setattr("pyprediktormapclient.dwh.pyodbc.drivers", lambda: [])
 
-    # Test failure scenario when no ODBC drivers are available
     with pytest.raises(ValueError) as excinfo:
         DWH(grs(), grs(), grs(), grs())
-    assert "is out of range" in str(excinfo.value)
+    assert "Driver index 0 is out of range." in str(excinfo.value)
